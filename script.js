2fd07e5e250f176354edd36b3e01c44dbc7d2896
Jonathan-Saenger <jonathan.saenger.pro@gmail.com>
2023-07-27 00:27:23 +0200

    Avancement du Footer

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
   
     // Apparition de la Navbar en format mobile
 
-    const openNav = document.querySelector(".menuBurger");
-    const navBar = document.querySelector("nav"); 
-    const closeNav = document.querySelector(".fermeture")
+const openNav = document.querySelector(".menuBurger");
+const navBar = document.querySelector("nav"); 
+const closeNav = document.querySelector(".fermeture")
 
-        openNav.addEventListener('click', apparitionMenu)
+    openNav.addEventListener('click', apparitionMenu)
 
-            function apparitionMenu () {
-                openNav.classList.toggle("active")
-                navBar.classList.toggle("active")
-            }
+        function apparitionMenu () {
+            openNav.classList.toggle("active")
+            navBar.classList.toggle("active")
+        }
 
-        closeNav.addEventListener('click', disparitionMenu)
+    closeNav.addEventListener('click', disparitionMenu)
 
-            function disparitionMenu () {
-                navBar.classList.remove("active")
-            }
+        function disparitionMenu () {
+            navBar.classList.remove("active")
+        } 
 
