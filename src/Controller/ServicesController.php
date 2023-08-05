0bca2d6b5ef8c3836a46a96cb403f08bd730f1b0
Jonathan-Saenger <jonathan.saenger@outlook.fr>
2023-08-06 00:02:43 +0200

    Avancement template rubrique Service

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -6,7 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Horaire;
+use App\Entity\Service;
 use App\Repository\HoraireRepository;
+use APP\Repository\ServiceRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 class ServicesController extends AbstractController
@@ -15,11 +17,15 @@ class ServicesController extends AbstractController
     public function index(ManagerRegistry $doctrine, HoraireRepository $HoraireRepository): Response
     {
         $HoraireRepository = $doctrine->getRepository(Horaire::class);
-        $Horaires = $HoraireRepository ->findAll();
+        $Horaires = $HoraireRepository->findAll();
+
+        $ServiceReposiroty = $doctrine->getRepository(Service::class);
+        $Services = $ServiceReposiroty->findAll();
 
         return $this->render('services/services.html.twig', [
             'controller_name' => 'ServicesController',
             'Horaires' => $Horaires,
+            'Services' => $Services,
         ]);
     }
 }
