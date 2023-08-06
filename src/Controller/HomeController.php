77e728d86ecb2e9cd796341fc1460fc7a583cc19
Jonathan-Saenger <jonathan.saenger@outlook.fr>
2023-08-06 23:58:57 +0200

    Fin du template service, fin de la pagination, début du témoignage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,25 +2,40 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Horaire;
+use App\Entity\Service;
+use App\Repository\HoraireRepository;
+use App\Repository\ServiceRepository;
+use Doctrine\Persistence\ManagerRegistry;
+//use Symfony\Component\BrowserKit\Request;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Persistence\ManagerRegistry;
-use App\Repository\HoraireRepository;
-use App\Entity\Horaire;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ManagerRegistry $doctrine, HoraireRepository $HoraireRepository): Response
+    public function index(ManagerRegistry $doctrine, HoraireRepository $HoraireRepository, ServiceRepository $ServiceRepository, Request $request, PaginatorInterface $paginatorInterface): Response
     {
         $HoraireRepository = $doctrine->getRepository(Horaire::class);
         $Horaires = $HoraireRepository ->findAll();
 
+        //$ServiceReposiroty = $doctrine->getRepository(Service::class);
+        //$Services = $ServiceReposiroty->findAll();
+
+        $Pagination = $paginatorInterface->paginate(
+            $ServiceRepository->paginationQuery(),
+            $request->query->get('page', 1),
+            3
+        );
 
         return $this->render('home/index.html.twig', [
             'controller_name' => ' Garage V.PARROT',
             'Horaires' => $Horaires,
+            //'Services' => $Services,
+            'Pagination' => $Pagination,
         ]);
     }
 }
--- src/Repository/ServiceRepository.php
+++ src/Repository/ServiceRepository.php
@@ -39,20 +39,13 @@ class ServiceRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Service[] Returns an array of Service objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function paginationQuery()
+    {
+        return $this->createQueryBuilder('s')
+            ->orderBy('s.id', 'ASC')
+            ->getQuery()
+        ;
+   }
 
 //    public function findOneBySomeField($value): ?Service
 //    {
