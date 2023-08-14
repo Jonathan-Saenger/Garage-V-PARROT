fb4891e9656b023285ecc0b26c197a96e4d4033b
Jonathan-Saenger <jonathan.saenger@outlook.fr>
2023-08-14 23:36:27 +0200

    Finalisation Template Temoignage

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -6,15 +6,35 @@ const navBar = document.querySelector("nav");
 const closeNav = document.querySelector(".fermeture")
 
     openNav.addEventListener('click', apparitionMenu)
-
         function apparitionMenu () {
             openNav.classList.toggle("active")
             navBar.classList.toggle("active")
         }
 
     closeNav.addEventListener('click', disparitionMenu)
-
         function disparitionMenu () {
             navBar.classList.remove("active")
-        } 
+        }
+        
+    //Formulaire témoignage
+
+    const etoiles = document.querySelectorAll(".boite-etoiles label");
+
+    etoiles.forEach((label, index) => {
+      label.addEventListener("click", () => {
+        for (let i = 0; i < etoiles.length; i++) {
+          if (i <= index) {
+            etoiles[i].classList.add("selected");
+          } else {
+            etoiles[i].classList.remove("selected");
+          }
+        }
+      });
+    });
+
+    
+    
+    
+    
+    
 
