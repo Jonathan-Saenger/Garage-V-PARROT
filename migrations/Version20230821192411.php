3df0ec1268ebd3d56700915718a75dc71746367e
Jonathan-Saenger <jonathan.saenger@outlook.fr>
2023-09-15 21:10:28 +0200

    suppression de la requête alter table horaire add admin_id .... (doublons)

--- migrations/Version20230821192411.php
+++ migrations/Version20230821192411.php
@@ -23,7 +23,6 @@ final class Version20230821192411 extends AbstractMigration
         $this->addSql('ALTER TABLE annonce ADD carburant VARCHAR(255) DEFAULT NULL, ADD boite_vitesse VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE horaire CHANGE admin_id admin_id INT DEFAULT NULL, CHANGE garage_id garage_id INT DEFAULT NULL, CHANGE heure_ouverture heure_ouverture TIME NOT NULL, CHANGE heure_fermeture heure_fermeture TIME NOT NULL, CHANGE ouverture_soir ouverture_soir TIME NOT NULL, CHANGE fermeture_soir fermeture_soir TIME NOT NULL');
         $this->addSql('ALTER TABLE temoignage CHANGE garage_id garage_id INT DEFAULT NULL, CHANGE note note INT NOT NULL, CHANGE jourpublication jourpublication DATE NOT NULL');
-        $this->addSql('ALTER TABLE horaire ADD admin_id INT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
