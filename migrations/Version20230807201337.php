76e1b7117b45afc6f2c70eafff91e246941cdd99
Jonathan-Saenger <jonathan.saenger@outlook.fr>
2023-09-17 00:20:38 +0200

    modification password mot de passe mogration

--- migrations/Version20230807201337.php
+++ migrations/Version20230807201337.php
@@ -20,8 +20,8 @@ final class Version20230807201337 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE admin ADD email VARCHAR(255) NOT NULL, ADD password VARCHAR(255) NOT NULL, DROP mail, DROP password');
-        $this->addSql('ALTER TABLE employe ADD email VARCHAR(255) NOT NULL, ADD password VARCHAR(255) NOT NULL, DROP mail, DROP password');
+        $this->addSql('ALTER TABLE admin ADD email VARCHAR(255) NOT NULL, ADD password VARCHAR(255) NOT NULL, DROP mail, DROP motdepasse');
+        $this->addSql('ALTER TABLE employe ADD email VARCHAR(255) NOT NULL, ADD password VARCHAR(255) NOT NULL, DROP mail, DROP motdepasse');
         $this->addSql('ALTER TABLE horaire CHANGE admin_id admin_id INT DEFAULT NULL, CHANGE garage_id garage_id INT DEFAULT NULL, CHANGE heure_ouverture heure_ouverture TIME DEFAULT NULL, CHANGE heure_fermeture heure_fermeture TIME DEFAULT NULL, CHANGE ouverture_soir ouverture_soir TIME DEFAULT NULL, CHANGE fermeture_soir fermeture_soir TIME DEFAULT NULL');
         $this->addSql('ALTER TABLE service CHANGE admin_id admin_id INT DEFAULT NULL, CHANGE garage_id garage_id INT DEFAULT NULL, CHANGE photo photo LONGBLOB NOT NULL');
         $this->addSql('ALTER TABLE temoignage CHANGE garage_id garage_id INT DEFAULT NULL');
